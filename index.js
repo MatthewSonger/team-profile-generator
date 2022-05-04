cfeda89bd9c6d9aa12f2609662f5f999d3c86c46
Matthew Songer <matt91893@gmail.com>
2022-05-04 17:48:12 -0500

    add engineer and intern questions

--- index.js
+++ index.js
@@ -102,3 +102,114 @@ const mangerQuestions = [
         }
     }
 ]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Employee Name (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please Enter A Name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Employee ID (Required)",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please Enter An Employee ID!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Employee Email Address (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please Enter An Employee Email Address!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubName',
+        message: "GitHub Username (Required)",
+        validate: githubName => {
+            if (githubName) {
+                return true;
+            } else {
+                console.log("Please Enter A GitHub Username!");
+                return false;
+            }
+        }
+    }
+    
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Employee Name (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please Enter A Name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Employee ID (Required)",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please Enter An Employee ID!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Employee Email Address (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please Enter An Employee Email Address!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'schoolName',
+        message: "Intern's School Name",
+        validate: schoolName => {
+            if (schoolName) {
+                return true;
+            } else {
+                console.log("Please Enter The Intern's School Name!");
+                return false;
+            }
+        }
+    }
+]

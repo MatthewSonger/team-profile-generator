efdc984b13d018b9583e312e22cde01a3aaa7ab1
Matthew Songer <matt91893@gmail.com>
2022-05-10 20:43:48 -0500

    add changes in wrong folder, redid adding icons and styles

--- src/generate-html.js
+++ src/generate-html.js
@@ -9,7 +9,7 @@ const generateHTML = function(data) {
                             <div class="card">
                             <div class="card-body bg-primary">
                                 <h5 class="card-title text-light">${element.getName()}</h5>
-                                <img src="./img/people.svg" class="glyph"></img><h6 class="text-light">${element.getRole()}</h3>
+                                <img src="./img/manager.svg" class="glyph"></img><h6 class="text-light">${element.getRole()}</h3>
                             </div>
                             <div class="card-footer">
                                 <ul class="list-group list-group-flush">
@@ -26,7 +26,7 @@ const generateHTML = function(data) {
                             <div class="card">
                             <div class="card-body bg-primary">
                                 <h5 class="card-title text-light">${element.getName()}</h5>
-                                <img src="./img/desktop.svg" class="glyph"></img><h6 class="text-light">${element.getRole()}</h3>
+                                <img src="./img/engineer.svg" class="glyph"></img><h6 class="text-light">${element.getRole()}</h3>
                             </div>
                             <div class="card-footer">
                             <ul class="list-group list-group-flush">
@@ -43,7 +43,7 @@ const generateHTML = function(data) {
                             <div class="card">
                             <div class="card-body bg-primary">
                                 <h5 class="card-title text-light">${element.getName()}</h5>
-                                <img src="./img/graduate.svg" class="glyph"></img><h6 class="text-light">${element.getRole()}</h3>
+                                <img src="./img/intern.svg" class="glyph"></img><h6 class="text-light">${element.getRole()}</h3>
                             </div>
                             <div class="card-footer">
                             <ul class="list-group list-group-flush">
@@ -78,11 +78,11 @@ function generatePage(array) {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                <link href="./style.css" rel="stylesheet">
+                <link href="./styles.css" rel="stylesheet">
                 <title>Team Roster</title>
             </head>
             <body>
-                <nav class="bg-danger text-light"><h2>Team Roster</h2></nav>
+                <nav class="bg-success text-light"><h2>Team Roster</h2></nav>
                 <div class="container">
                     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                         ${appendCards(array)}
